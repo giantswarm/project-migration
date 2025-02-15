01a2097f38da1d57d917e99ddf8a76ad80c1e428
Timo Derstappen <teemow@gmail.com>
2025-02-15 12:22:52 +0100

    Forgot: Move types into its own package.

---
+++ types/types.go
@@ -0,0 +1,47 @@
+package types
+
+// Data types for JSON responses.
+type Project struct {
+	Number int    `json:"number"`
+	ID     string `json:"id"`
+}
+
+type ProjectList struct {
+	Projects []Project `json:"projects"`
+}
+
+type Option struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type Field struct {
+	ID      string   `json:"id"`
+	Name    string   `json:"name"`
+	Options []Option `json:"options"`
+}
+
+type FieldResponse struct {
+	Fields []Field `json:"fields"`
+}
+
+type Content struct {
+	URL   string `json:"url"`
+	Type  string `json:"type"`
+	Title string `json:"title"`
+}
+
+type Item struct {
+	ID         string  `json:"id"`
+	Title      string  `json:"title"`
+	Content    Content `json:"content"`
+	Status     string  `json:"status"`
+	Kind       string  `json:"kind"`
+	Workstream string  `json:"workstream"`
+	StartDate  string  `json:"start Date"`
+	TargetDate string  `json:"target Date"`
+}
+
+type ItemList struct {
+	Items []Item `json:"items"`
+}
